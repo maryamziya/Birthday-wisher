785d2ed7f19a92fb828a2fc5d51aff4f292c8a12
ziya <maryamkumbe@gmail.com>
2025-11-27 18:09:47 -0800

    updated

--- main.py
+++ main.py
@@ -13,22 +13,8 @@ import pandas
 # 1. Update the birthdays.csv with your friends & family's details. 
 # HINT: Make sure one of the entries matches today's date for testing purposes. 
 dataframe = pandas.read_csv("birthdays.csv")
-
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# HINT 1: Only the month and day matter. 
-# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
-# birthdays_dict = {
-#     (month, day): data_row
-# }
 birthdays_dict = dataframe.set_index(["month", "day"]).to_dict("index")
 letter_templates_list = []
-#
-
-# dict_list = dataframe.to_dict(orient="records")
-# for item in
-#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
 now = datetime.datetime.now()
 today_month = now.month
 today_day = now.day
@@ -51,6 +37,21 @@ if (today_month, today_day) in birthdays_dict:
                 to_addrs=to_address,
                 msg=f"Subject:HAPPY BIRTH DAY!!!\n\n{letter}"
             )
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# HINT 1: Only the month and day matter. 
+# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
+# birthdays_dict = {
+#     (month, day): data_row
+# }
+
+#
+
+# dict_list = dataframe.to_dict(orient="records")
+# for item in
+#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
+
 
 
 
